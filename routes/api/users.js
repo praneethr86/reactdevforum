7e86899c5cae216c7e47d24c2e40d12f259fbc78
Praneeth Reddy <praneeth001@gmail.com>
2019-08-17 17:53:43 +0530

    Added: config and MongoDB connection via Mongoose Added: routes for posts, users, auth, profile Modified: server.js for using the routes Added: basic userSchema model via mongoose (not used yet)

---
+++ routes/api/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/Users
+// @desc    Test route
+// @access   public
+router.get('/', (req, res) => res.send('User route'));
+
+module.exports = router;
